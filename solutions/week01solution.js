74754ec5dd01c9398e92377941aa4b96d93faf1e
Aaron Hill <aaron.hill@me.com>
2016-08-30 13:48:02 -0400

    getting set up for first week of fall 2016 semester

--- solutions/week01solution.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-var fs = require('fs');
-
-request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=02&borough=M', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    fs.writeFileSync('/home/ubuntu/workspace/data/m02meetings.txt', body);
-  }
-  else {console.error('request failed')}
-})
