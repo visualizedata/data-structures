724962517166ab5ea39797f45a948d8606fa6888
Aaron Hill <aaron.hill@me.com>
2018-11-26 08:23:04 -0500

    add fa example

---
+++ assignments/final_assignment_example/server.js
@@ -0,0 +1,172 @@
+var express = require('express'), // npm install express
+    app = express();
+const { Pool } = require('pg');
+var AWS = require('aws-sdk');
+const moment = require('moment'); // moment-timezone --save
+
+// AWS RDS credentials
+var db_credentials = new Object();
+db_credentials.user = 'aaron';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'mydb';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// respond to requests for /sensor
+app.get('/sensor', function(req, res) {
+    
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query 
+    var q = `SELECT EXTRACT(DAY FROM sensorTime) as sensorday,
+             AVG(sensorValue::int) as num_obs
+             FROM sensorData
+             GROUP BY sensorday
+             ORDER BY sensorday;`;
+
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        else {
+            res.send(qres.rows);
+            client.end();
+            console.log('1) responded to request for sensor data');
+        }
+    });
+});
+
+// respond to requests for /aameetings
+app.get('/aameetings', function(req, res) {
+    
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+    
+    // SQL query 
+    var thisQuery = `SELECT mtgaddress, mtglocation as location, json_agg(json_build_object('day', mtgday, 'time', mtgtime)) as meetings
+                 FROM aadata 
+                 WHERE mtgday = 'Tuesday' and mtghour >= 19 
+                 GROUP BY mtgaddress, mtglocation
+                 ;`;
+
+    client.query(thisQuery, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        else {
+            res.send(qres.rows);
+            client.end();
+            console.log('2) responded to request for aa meeting data');
+        }
+    });
+});
+
+// respond to requests for /deardiary
+app.get('/deardiary', function(req, res) {
+    
+    // AWS DynamoDB credentials
+    AWS.config = new AWS.Config();
+    AWS.config.accessKeyId = process.env.AWS_ID;
+    AWS.config.secretAccessKey = process.env.AWS_KEY;
+    AWS.config.region = "us-east-1";
+
+    // Connect to the AWS DynamoDB database
+    var dynamodb = new AWS.DynamoDB();
+    
+    // DynamoDB (NoSQL) query
+    var params = {
+        TableName: "aarondiary",
+        KeyConditionExpression: "#tp = :topicName", // the query expression
+        ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+            "#tp": "topic"
+        },
+        ExpressionAttributeValues: { // the query values
+            ":topicName": { S: "cats" }
+        }
+    };
+
+    dynamodb.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        }
+        else {
+            res.send(data.Items);
+            console.log('3) responded to request for dear diary data');
+        }
+    });
+});
+
+// create templates
+var hx = `<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>AA Meetings</title>
+  <meta name="description" content="Meetings of AA in Manhattan">
+  <meta name="author" content="AA">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+   crossorigin=""/>
+</head>
+<body>
+<div id="mapid"></div>
+  <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+   integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+   crossorigin=""></script>
+  <script>
+  var data = 
+  `;
+  
+var jx = `;
+    var mymap = L.map('mapid').setView([40.734636,-73.994997], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        // accessToken: 'your.mapbox.access.token'
+        accessToken: 'pk.eyJ1Ijoidm9ucmFtc3kiLCJhIjoiY2pveGF0aDV2MjIyOTNsbWxlb2hhMmR4dCJ9.JJdYD_jWgRwUeJkDWiBz3w'
+    }).addTo(mymap);
+
+    for (var i=0; i<data.length; i++) {
+        L.marker( [data[i].lat, data[i].lon] ).bindPopup(JSON.stringify(data[i].meetings)).addTo(mymap);
+    }
+    </script>
+    </body>
+    </html>`;
+
+// respond to requests for /aameetings
+app.get('/aa', function(req, res) {
+
+    var now = moment(Date.now()); 
+    var dayy = now.day().toString(); 
+    var hourr = now.hour().toString(); 
+
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+    
+    // SQL query 
+    var thisQuery = `SELECT lat, lon, json_agg(json_build_object('loc', mtglocation, 'address', mtgaddress, 'time', tim, 'name', mtgname, 'day', day, 'types', types, 'shour', shour)) as meetings
+                 FROM aadatall 
+                 WHERE day = ` + dayy + 'and shour >= ' + hourr + 
+                 `GROUP BY lat, lon
+                 ;`;
+
+    client.query(thisQuery, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        
+        else {
+            var resp = hx + JSON.stringify(qres.rows) + jx;
+            res.send(resp);
+            client.end();
+            console.log('2) responded to request for aa meeting data');
+        }
+    });
+});
+
+// serve static files in /public
+app.use(express.static('public'));
+
+// listen on port 8080
+app.listen(8080, function() {
+    console.log('Server listening...');
+});
