c4cb8c3fbee461da7f74d41471eb91ea4f11bacc
Aaron Hill <aaron@aarons-mbp.myfiosgateway.com>
2020-10-07 10:54:46 -0400

    update dyndb data sample

--- weekly_assignment_06/addToDynamo.js
+++ weekly_assignment_06/addToDynamo.js
@@ -14,11 +14,11 @@ class DiaryEntry {
 diaryEntries.push(new DiaryEntry('personal', 'March 10, 1976 11:30:00', "I was born!"));
 diaryEntries.push(new DiaryEntry('personal', 'October 31, 2015 13:00:00', "I piloted my first solo flight! I ate pancakes to celebrate."));
 diaryEntries.push(new DiaryEntry('work', 'June 1, 1998 08:00:00', "I started my first professional job."));
-diaryEntries.push(new DiaryEntry('work', 'September 25, 2019 12:10:00', "I taught my favorite students."));
-diaryEntries.push(new DiaryEntry('cats', 'September 26, 2019 23:00:00', "Eudora chased his tail."));
-diaryEntries.push(new DiaryEntry('work', 'October 2, 2019 12:10:00', "I taught my favorite students."));
-diaryEntries.push(new DiaryEntry('cats', 'October 3, 2019 22:15:00', "Mr. Jingles took a nap."));
-diaryEntries.push(new DiaryEntry('work', 'October 9, 2019 12:10:00', "Class doesn't meet."));
+diaryEntries.push(new DiaryEntry('work', 'September 23, 2020 12:10:00', "I taught my favorite students."));
+diaryEntries.push(new DiaryEntry('cats', 'September 26, 2020 23:00:00', "Eudora chased his tail."));
+diaryEntries.push(new DiaryEntry('work', 'October 7, 2020 12:10:00', "I taught my favorite students."));
+diaryEntries.push(new DiaryEntry('cats', 'October 6, 2020 22:15:00', "Mr. Jingles took a nap."));
+diaryEntries.push(new DiaryEntry('work', 'November 25, 2020 12:10:00', "Class doesn't meet."));
 
 console.log(diaryEntries);
 
